7a8a936ce0030ee24e8307a8b03506fc371f7115
asrafulhaq <devvorb@gmail.com>
2023-08-07 23:08:02 +0600

    Update

--- student-result-system/src/functions.js
+++ student-result-system/src/functions.js
@@ -87,3 +87,83 @@ const getRandomUniqueID = (length = 10) => {
 
   return result;
 };
+
+const getGpaGrade = (marks) => {
+  let gpa;
+  let grade;
+
+  if (marks >= 0 && marks < 33) {
+    gpa = 0;
+    grade = "F";
+  } else if (marks >= 33 && marks < 40) {
+    gpa = 1;
+    grade = "D";
+  } else if (marks >= 40 && marks < 50) {
+    gpa = 2;
+    grade = "C";
+  } else if (marks >= 50 && marks < 60) {
+    gpa = 3;
+    grade = "B";
+  } else if (marks >= 60 && marks < 70) {
+    gpa = 3.5;
+    grade = "A-";
+  } else if (marks >= 70 && marks < 80) {
+    gpa = 4;
+    grade = "A";
+  } else if (marks >= 80 && marks <= 100) {
+    gpa = 5;
+    grade = "A+";
+  }
+  return {
+    gpa: gpa,
+    grade: grade,
+  };
+};
+
+const getFinalResult = (marks) => {
+  let cgpa;
+  let result;
+
+  let totalGpa =
+    getGpaGrade(marks.bangla).gpa +
+    getGpaGrade(marks.english).gpa +
+    getGpaGrade(marks.math).gpa +
+    getGpaGrade(marks.science).gpa +
+    getGpaGrade(marks.social_science).gpa +
+    getGpaGrade(marks.religion).gpa;
+
+  cgpa = totalGpa / 6;
+
+  if (
+    marks.bangla >= 33 &&
+    marks.english >= 33 &&
+    marks.math >= 33 &&
+    marks.science >= 33 &&
+    marks.social_science >= 33 &&
+    marks.religion >= 33
+  ) {
+    if (cgpa >= 1 && cgpa < 2) {
+      result = "D";
+    } else if (cgpa >= 2 && cgpa < 3) {
+      result = "C";
+    } else if (cgpa >= 3 && cgpa < 3.5) {
+      result = "B";
+    } else if (cgpa >= 3.5 && cgpa < 4) {
+      result = "A-";
+    } else if (cgpa >= 4 && cgpa < 5) {
+      result = "A";
+    } else if (cgpa >= 5) {
+      result = "A+";
+    }
+
+    return {
+      result: result,
+      cgpa: cgpa,
+    };
+  } else {
+    return {
+      result: "F",
+      cgpa: cgpa,
+    };
+  }
+};
--- student-result-system/src/search.js
+++ student-result-system/src/search.js
@@ -1,4 +1,5 @@
 const search_result_form = document.getElementById("search_result_form");
+const sheet = document.querySelector(".student-result-sheet");
 
 search_result_form.onsubmit = (e) => {
   e.preventDefault();
@@ -6,13 +7,108 @@ search_result_form.onsubmit = (e) => {
   const form_data = new FormData(e.target);
   const data = Object.fromEntries(form_data.entries());
 
-  console.log(data);
-
   let oldData = getDataLS("students");
 
   const studentResult = oldData.find(
     (item) => item.roll === data.roll && item.reg === data.reg
   );
 
-  console.log(studentResult);
+  let content;
+
+  if (studentResult) {
+    content = `
+    <div class="student-info">
+    <img
+      style="width: 150px; height: 150px"
+      class="shadow"
+      src="${studentResult.photo}"
+      alt=""
+    />
+    <h3>${studentResult.name}</h3>
+    <p>Roll : ${studentResult.roll} | Reg: ${studentResult.reg}</p>
+    ${
+      getFinalResult({
+        bangla: studentResult.result.bangla,
+        english: studentResult.result.english,
+        math: studentResult.result.math,
+        science: studentResult.result.science,
+        social_science: studentResult.result.social_science,
+        religion: studentResult.result.religion,
+      }).result === "F"
+        ? '<h2 style="color:red;">Failed</h2>'
+        : '<h2 style="color:green;">Passed</h2>'
+    }
+
+    <hr />
+    <table class="table table-bordered">
+      <tr>
+        <td>Subject</td>
+        <td>Marks</td>
+        <td>GPA</td>
+        <td>Grade</td>
+        <td>CGPA</td>
+        <td>Final Result</td>
+      </tr>
+      <tr>
+        <td>Bangla</td>
+        <td>${studentResult.result.bangla}</td>
+        <td>${getGpaGrade(studentResult.result.bangla).gpa}</td>
+        <td>${getGpaGrade(studentResult.result.bangla).grade}</td>
+        <td rowspan="6">${getFinalResult({
+          bangla: studentResult.result.bangla,
+          english: studentResult.result.english,
+          math: studentResult.result.math,
+          science: studentResult.result.science,
+          social_science: studentResult.result.social_science,
+          religion: studentResult.result.religion,
+        }).cgpa.toFixed(2)}</td>
+        <td rowspan="6">${
+          getFinalResult({
+            bangla: studentResult.result.bangla,
+            english: studentResult.result.english,
+            math: studentResult.result.math,
+            science: studentResult.result.science,
+            social_science: studentResult.result.social_science,
+            religion: studentResult.result.religion,
+          }).result
+        }</td>
+      </tr>
+      <tr>
+        <td>English</td>
+        <td>${studentResult.result.english}</td>
+        <td>${getGpaGrade(studentResult.result.english).gpa}</td>
+        <td>${getGpaGrade(studentResult.result.english).grade}</td>
+      </tr>
+      <tr>
+        <td>Math</td>
+        <td>${studentResult.result.math}</td>
+        <td>${getGpaGrade(studentResult.result.math).gpa}</td>
+        <td>${getGpaGrade(studentResult.result.math).grade}</td>
+      </tr>
+      <tr>
+        <td>Science</td>
+        <td>${studentResult.result.science}</td>
+        <td>${getGpaGrade(studentResult.result.science).gpa}</td>
+        <td>${getGpaGrade(studentResult.result.science).grade}</td>
+      </tr>
+      <tr>
+        <td>Social Science</td>
+        <td>${studentResult.result.social_science}</td>
+        <td>${getGpaGrade(studentResult.result.social_science).gpa}</td>
+        <td>${getGpaGrade(studentResult.result.social_science).grade}</td>
+      </tr>
+      <tr>
+        <td>Religion</td>
+        <td>${studentResult.result.religion}</td>
+        <td>${getGpaGrade(studentResult.result.religion).gpa}</td>
+        <td>${getGpaGrade(studentResult.result.religion).grade}</td>
+      </tr>
+    </table>
+  </div>
+    `;
+  } else {
+    content = "Result not found";
+  }
+
+  sheet.innerHTML = content;
 };
